7dc27169d3bd920e101749efe7acd4a7e91eac17
Vipin Yadav <imvpn22@gmail.com>
2018-12-10 21:12:20 +0530

    Updates

---
+++ precache-manifest.cb08f64249c6e4d795eda0acc3a82ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff5599c6d58809ffcea6",
+    "url": "/static/js/main.ff5599c6.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "ff5599c6d58809ffcea6",
+    "url": "/static/css/main.c8ddc4c5.chunk.css"
+  },
+  {
+    "revision": "1d66012367c31ea8a2b86fd94d0f5b37",
+    "url": "/index.html"
+  }
+];
