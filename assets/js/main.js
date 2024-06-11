c7dd7809cb2ff39cfdbd5fd56d844e7593ad463f
Vipin Yadav <imvpn22@gmail.com>
2024-06-11 13:53:29 +0530

    dp update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,22 @@
-var handleLink = bodyDOM => {
-  $(document).on('click', "a[href^='#']", event => {
+var handleLink = (bodyDOM) => {
+  $(document).on("click", "a[href^='#']", (event) => {
     // Prevent default link handling
     event.preventDefault();
 
     let $link = $(event.target);
-    let $target = $($link.attr('href'));
+    let $target = $($link.attr("href"));
 
     // Scroll to give target
     bodyDOM.stop().animate(
       {
-        scrollTop: $target.offset().top
+        scrollTop: $target.offset().top,
       },
       1000,
-      'easeInOutExpo'
+      "easeInOutExpo"
     );
   });
 };
 
-window.addEventListener('load', event => {
-  handleLink($('html'));
-
-  // Close popup
-  document.getElementById('popupCloseBtn').addEventListener('click', () => {
-    document.getElementById('popupOverlay').classList.add('hidden');
-  });
+window.addEventListener("load", (event) => {
+  handleLink($("html"));
 });
